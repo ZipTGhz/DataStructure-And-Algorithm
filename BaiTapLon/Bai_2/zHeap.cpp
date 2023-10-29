38e94b35c95e0eb4c448fc7d5c4359daeb05f7b3
ZipTGhz <gipvosongf@gmail.com>
2023-10-29 20:22:07 +0700

    Cập nhật zHeap
    Cập nhật zHeap như một hàng đợi ưu tiên và giải bài tập nối thanh kim loại.

--- BaiTapLon/Bai_2/zHeap.cpp
+++ BaiTapLon/Bai_2/zHeap.cpp
@@ -2,39 +2,40 @@
 
 using namespace std;
 
-template <class T>
+template <class T, class Comparator = less<T>>
 class zHeap
 {
 private:
     zVector<T> heap;
-
-    void max_zHeap(size_t children)
+    Comparator comp;
+    void adjust_zHeap(size_t children)
     {
         size_t parent = children >> 1;
         if (parent < 1)
             return;
-        if (heap[children] > heap[parent])
+        if (!comp(heap[children], heap[parent]))
         {
             swap(heap[children], heap[parent]);
-            max_zHeap(parent);
+            adjust_zHeap(parent);
         }
     }
 
-    void reverse_max_zHeap(size_t parent)
+    void reverse_adjust_zHeap(size_t parent)
     {
         size_t larger;
         size_t left = parent << 1, right = (parent << 1) + 1;
-        if (right < heap.size() && heap[parent] < heap[right])
+
+        if (right < heap.size() && comp(heap[parent], heap[right]))
             larger = right;
         else
             larger = parent;
-        if (left < heap.size() && heap[parent] < heap[left])
+        if (left < heap.size() && comp(heap[parent], heap[left]))
             larger = left;
 
         if (larger != parent)
         {
             swap(heap[larger], heap[parent]);
-            reverse_max_zHeap(larger);
+            reverse_adjust_zHeap(larger);
         }
     }
 
@@ -44,62 +45,48 @@ public:
     void push(T __data)
     {
         heap.push_back(__data);
-        max_zHeap(heap.size() - 1);
+        adjust_zHeap(heap.size() - 1);
     }
 
     void pop()
     {
         heap[1] = heap[heap.size() - 1];
         heap.pop_back();
-        reverse_max_zHeap(1);
-    }
-
-    void print_max_zHeap(size_t parent = 1)
-    {
+        reverse_adjust_zHeap(1);
     }
 
     size_t size() { return heap.size() - 1; }
     size_t size() const { return heap.size() - 1; }
-    T operator[](size_t i) { return heap[i + 1]; }
-    T operator[](size_t i) const { return heap[i + 1]; }
     T top() { return heap[1]; }
+    T top() const { return heap[1]; }
+    bool empty() { return heap.size() == 1; }
+    bool empty() const { return heap.size() == 1; }
 };
-
-template <class T>
-bool is_max_zHeap(const zHeap<T> &heap)
-{
-    size_t left, right;
-    for (size_t parent = (heap.size() - 1) >> 1; parent >= 1; parent--)
-    {
-        left = parent << 1, right = (parent << 1) + 1;
-        if (heap[left] > heap[parent] || heap[right] > heap[parent])
-            return false;
-    }
-    return true;
-}
 
 int main()
 {
-    zHeap<int> test;
-    zVector<int> arr = {35, 33, 42, 10, 14, 19, 27, 44, 26, 31};
-    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+    int n;
+    cin >> n;
+    zHeap<int, greater<int>> myPQueue;
+    while (n--)
     {
-        test.push(arr[i]);
+        int x;
+        cin >> x;
+        myPQueue.push(x);
     }
 
-    for (int i = 0; i < test.size(); i++)
-    {
-        cout << test[i] << " ";
-    }
-    cout << endl;
-    test.pop();
+    int res = 0;
 
-    for (int i = 0; i < test.size(); i++)
+    while (myPQueue.size() != 1)
     {
-        cout << test[i] << " ";
+        int first = myPQueue.top();
+        myPQueue.pop();
+        int second = myPQueue.top();
+        myPQueue.pop();
+        int sum = first + second;
+        res += sum;
+        myPQueue.push(sum);
     }
-
-    cout << endl;
-    cout << is_max_zHeap(test);
+    cout << res;
     return 0;
 }
